a45509a0575cb4d22a392ac7461678f6d60ff284
Kvilarinho <katia.vilas@gmail.com>
2025-12-16 20:57:45 +0000

    Initilize project - already started html, css, js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function showExperienceDetails() {
+    const details = document.getElementById('experience-details');
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+};
+
+function showAboutDetails() {
+    const aboutDetails = document.getElementById('about-details');
+    if (aboutDetails.style.display === 'none' || aboutDetails.style.display === '') {
+        aboutDetails.style.display = 'block';
+    } else {
+        aboutDetails.style.display = 'none';
+    }
+}
+
