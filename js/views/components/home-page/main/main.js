c989ace7ac17c8778c434d61f9feb51b873def3c
Kvilarinho <katia@katiavilarinho.dev>
2026-02-04 21:19:19 +0000

    Add project section and card components: implement project display with descriptions and links

--- js/views/components/home-page/main/main.js
+++ js/views/components/home-page/main/main.js
@@ -2,6 +2,7 @@ import sectionHero from "/js/views/components/home-page/main/section-hero.js";
 import sectionAbout from "/js/views/components/home-page/main/section-about.js";
 import sectionExperience from "/js/views/components/home-page/main/section-experience.js";
 import sectionEducation from "/js/views/components/home-page/main/section-education.js";
+import sectionProjects from "/js/views/components/home-page/main/section-projects.js";
 
 export default function main() {
     const main = document.createElement("main");
@@ -12,11 +13,13 @@ export default function main() {
     const sectionAboutElement = sectionAbout();
     const sectionExperienceElement = sectionExperience();
     const sectionEducationElement = sectionEducation();
+    const sectionProjectsElement = sectionProjects();
 
     main.appendChild(sectionHeroElement);
     main.appendChild(sectionAboutElement);
     main.appendChild(sectionExperienceElement);
     main.appendChild(sectionEducationElement);
+    main.appendChild(sectionProjectsElement);
     
     return main;
 }
