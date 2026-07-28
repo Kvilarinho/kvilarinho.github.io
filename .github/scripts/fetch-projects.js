19e54004dd1e89ed50b67a0db8832c36e3095a17
Kvilarinho <katia@katiavilarinho.dev>
2026-07-28 19:09:22 +0100

    Add custom titles and images for projects

--- .github/scripts/fetch-projects.js
+++ .github/scripts/fetch-projects.js
@@ -5,7 +5,22 @@ const USERNAME = 'Kvilarinho'
 const TOKEN = process.env.GITHUB_TOKEN
 
 const CUSTOM_TITLES = {
-  'kvilarinho.github.io': 'Personal Portfolio Website'
+  'kvilarinho.github.io': 'Personal Portfolio Website',
+  'Concurrent-TCP-ChatServer': 'Concurrent TCP Chat',
+  'Vanilla-Webserver': 'Vanilla HTTP Web Server',
+  'Task-Manager': 'Task Manager (CLI)',
+  'FinanceTracker': 'Finance Tracker (CLI)',
+  'LibraryBookTracker': 'Library Book Tracker (CLI)'
+}
+
+const CUSTOM_IMAGES = {
+  'kvilarinho.github.io': '/img/projects/portfolio-website/portfolio-website.png',
+  'Study-Clarify': '',
+  'Concurrent-TCP-ChatServer': '/img/projects/concurrent-tcp-chatserver/concurrent-tcp-chatserver.jpeg',
+  'Vanilla-Webserver': '/img/projects/vanilla-webserver/vanilla-webserver.jpeg',
+  'Task-Manager': '/img/projects/task-manager/task-manager.jpeg',
+  'FinanceTracker': '/img/projects/finance-tracker/Finance Tracker.jpg',
+  'LibraryBookTracker': '/img/projects/library-book-tracker/Library Book Tracker.jpg'
 }
 
 function get(url) {
@@ -49,6 +64,7 @@ async function main() {
       github: repo.html_url,
       stars: repo.stargazers_count,
       updatedAt: repo.updated_at,
+      image: CUSTOM_IMAGES[repo.name] || '',
       readme
     })
   }
